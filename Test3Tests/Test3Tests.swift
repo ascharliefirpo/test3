7f3f13f8655f447ab76c63abb83841a3d6304bfb
Andrea Sgariglia <andresgari@tiscali.it>
2016-06-28 11:19:41 +0200

    Initial Commit

---
+++ Test3Tests/Test3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Test3Tests.swift
+//  Test3Tests
+//
+//  Created by Utente on 28/06/16.
+//  Copyright © 2016 Asgar. All rights reserved.
+//
+
+import XCTest
+@testable import Test3
+
+class Test3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
